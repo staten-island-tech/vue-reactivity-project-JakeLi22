ba2c5f63357f9ff5c08c8c605b15ffe05ba316a7
JakeLi22 <56609057+JakeLi22@users.noreply.github.com>
2021-10-21 12:26:17 -0400

    basic setup

---
+++ reactivity-prj/src/views/Project.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="title">
+    <h1>This is a new title</h1>
+    <button class="click-button">Clik me!</button>
+  </div>
+  
+</template>
+
+<template>
+
+
+<style lang='css'>
+.title{
+   background-color:red; 
+}
+</style>
+
+<script>
+data {
+  return {
+    dfs:j 'fdsf'
+  }
+}
+</script>
