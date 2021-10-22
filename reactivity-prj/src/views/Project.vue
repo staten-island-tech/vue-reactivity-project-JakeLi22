0e7b4506d6dc1e3eda6360588546a748f9e50c54
JakeLi22 <56609057+JakeLi22@users.noreply.github.com>
2021-10-22 12:30:06 -0400

    figure out methods
    not working

--- reactivity-prj/src/views/Project.vue
+++ reactivity-prj/src/views/Project.vue
@@ -2,22 +2,30 @@
   <div class="title">
     <h1>Birthday Logger</h1>
     <div class="birthday-data-div">
-
+      <h2>name:</h2>
+      <h2>mm/dd</h2>
+      <h2>days remaining</h2>
     </div>
-    <button class="click-button">Add B-day!</button>
+    <button class="click-button" v-on:click= 'test'>Add B-day!</button>
   </div>
   
 </template>
 
-<template>
 
 
-<style lang='css'>
+<style >
 .title{
-   background-color:red; 
 }
 </style>
 
 <script>
+Vue.component("birthday-data", {
 
+
+methods:{ 
+  test() {
+    console.log('clicked')
+  }
+}
+}) 
 </script>
