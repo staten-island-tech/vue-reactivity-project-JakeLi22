9e14c9a776543b2380be623d170abd40eb1bb06d
Jake Li <jake.li22@sitechhs.com>
2021-10-25 08:47:25 -0400

    k

--- reactivity-prj/src/views/Home.vue
+++ reactivity-prj/src/views/Home.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="home">
-    <h1>this is a new title </h1>
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>this is a new title</h1>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
