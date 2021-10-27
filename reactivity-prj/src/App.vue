843b372b415fc677dadf8ccc0962ea8c59fe37d8
JakeLi22 <56609057+JakeLi22@users.noreply.github.com>
2021-10-27 11:46:11 -0400

    data and methods added

--- reactivity-prj/src/App.vue
+++ reactivity-prj/src/App.vue
@@ -2,11 +2,13 @@
   <div id="container">
     <h1 class="website-title">Battery Cells</h1>
     <div class="cell-options">
+     
       <!-- 
         img
         list
         cell name  
-        quantity
+        quantity 2 button with plus and minus
+          v-on:click="name of the function"
         price
 
 
@@ -15,6 +17,7 @@
 <button class="cart-button">Add to Cart</button>
 <!-- see cart
 emit to add a number to the cart(#)
+.value of the box and 
  -->
   </div>
 </template>
@@ -26,10 +29,36 @@ emit to add a number to the cart(#)
 </style>
 
 <script>
-export as {
-data() {
+export default {
+data(){
   return {
-    test: true,
+    cellType: "lithium-ion", 
+    seller: "panasonic",
+    types: [
+      {
+        cellId: 1, 
+        cellName: "", 
+        details: ["mAh","V"],
+        cellImage: "", 
+        cellPrice:1, 
+        
+      },
+      {
+          cellId: 2, 
+        cellName: "", 
+        details: ["mAh","V"],
+        cellImage: "", 
+        cellPrice:2, 
+        
+      }
+    ]
   }
-};}
+},
+  methods: {
+    increaseQuantity(){
+      //this.name of the element += 1
+    }
+
+}
+}
 </script>
